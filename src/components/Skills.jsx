f1ec4b7ab70e232c415db06d5a354d3152bfe023
Docoppolis <j.parrott653@gmail.com>
2025-11-03 21:45:02 -0500

    Finished skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,6 +3,17 @@ import cpp from "../assets/CPP.png";
 import react from "../assets/React.png";
 import micronaut from "../assets/Micronaut.png"
 import unrealEngine from "../assets/UnrealEngine.png"
+import node from "../assets/Node.png";
+import git from "../assets/Git.png";
+import gitHub from "../assets/GitHub.png";
+import python from "../assets/Python.png";
+import postgreSQL from "../assets/PostgreSQL.png";
+import scrum from "../assets/Scrum.png";
+import cs from "../assets/CS.png";
+import postman from "../assets/Postman.png";
+import sql from "../assets/SQL.png";
+import java from "../assets/Java.png";
+import next from "../assets/Next.png";
 
 export default function Skills() {
     return(
@@ -35,19 +46,59 @@ export default function Skills() {
             </div>
 
             <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
-                <img className="w-16 h-9 mb-1" src={docker} alt="Docker logo"></img>
-                <h1 className="text-slate-100 font-bold text-xs">Docker</h1>
+                <img className="w-16 h-9 mb-1" src={node} alt="Node.js logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Node.js</h1>
             </div>
 
             <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
-                <img className="w-16 h-9 mb-1" src={docker} alt="Docker logo"></img>
-                <h1 className="text-slate-100 font-bold text-xs">Docker</h1>
+                <img className="w-14 h-9 mb-1" src={git} alt="Git logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Git</h1>
             </div>
 
             <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
-                <img className="w-16 h-9 mb-1" src={docker} alt="Docker logo"></img>
-                <h1 className="text-slate-100 font-bold text-xs">Docker</h1>
-            </div>                        
+                <img className="w-16 h-9 mb-1" src={gitHub} alt="GitHub logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">GitHub</h1>
+            </div>
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-16 h-9 mb-1" src={python} alt="Python logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Python</h1>
+            </div>   
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-12 h-9 mb-1" src={postgreSQL} alt="PostgreSQL logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">PostgreSQL</h1>
+            </div>  
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-12 h-9 mb-1" src={scrum} alt="Scrum logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Agile/Scrum</h1>
+            </div>   
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-9 h-9 mb-1" src={cs} alt="C# logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">C#</h1>
+            </div>           
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-9 h-9 mb-1" src={postman} alt="Postman logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Postman</h1>
+            </div>  
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-9 h-9 mb-1" src={sql} alt="SQL logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">SQL</h1>
+            </div>  
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-12 h-9 mb-1" src={java} alt="Java logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Java</h1>
+            </div>  
+
+            <div className="flex flex-col items-center justify-center w-40 h-20 rounded-2xl bg-transparent border border-violet-500 transition-all duration-300 ease-in-out hover:scale-[1.08] hover:shadow-[0_0_0_3px_rgba(139,92,246,0.6)] hover:bg-slate-800/75">
+                <img className="w-9 h-9 mb-1" src={next} alt="Next.js logo"></img>
+                <h1 className="text-slate-100 font-bold text-xs">Next.js</h1>
+            </div>                                                                                                           
 
         </div>
 
